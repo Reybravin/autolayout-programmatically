ff7a2431baa365c2e70024899d13dbfa9c197c71
Sergey Sachuk <reybravin@gmail.com>
2018-03-28 13:27:59 +0300

    initial commit

---
+++ autolayout-programatically/Extensions/SwipingController+extension.swift
@@ -0,0 +1,31 @@
+//
+//  SwipingController+extension.swift
+//  autolayout-programatically
+//
+//  Created by Sergiy Sachuk on 1/18/18.
+//  Copyright © 2018 Sergiy Sachuk. All rights reserved.
+//
+
+import UIKit
+
+extension SwipingController {
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        coordinator.animate(alongsideTransition: { (_) in
+            self.collectionViewLayout.invalidateLayout()
+            
+            if self.pageControl.currentPage == 0 {
+                self.collectionView?.contentOffset = .zero
+            } else {
+                let indexPath = IndexPath(item: self.pageControl.currentPage, section: 0)
+                self.collectionView?.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            }
+            
+        }) { (_) in
+            
+        }
+    }
+    
+}
+
---
+++ autolayout-programatically/Model/Page.swift
@@ -0,0 +1,17 @@
+//
+//  Page.swift
+//  autolayout-programatically
+//
+//  Created by Sergiy Sachuk on 1/18/18.
+//  Copyright © 2018 Sergiy Sachuk. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    
+    let imageName: String
+    let headerText: String
+    let bodyText: String
+    
+}
